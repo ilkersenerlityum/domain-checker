d579c2d27c68a6ab367746aebd8bf2409127f587
ilkersenerlityum <ilker@lityum.com>
2025-03-11 12:24:31 +0300

    request update2

--- cypress/e2e/request.cy.js
+++ cypress/e2e/request.cy.js
@@ -2,23 +2,25 @@ const { exec } = require('child_process');
 
 describe('Domain Content Check', () => {
   const domains = [
-    { url: 'https://demo.peoplebox.biz/user/login', selector: '._main_1p1ww_22'},
-    { url: 'https://demo2.peoplebox.biz/user/login', selector: '._main_1p1ww_22'},
-    { url: 'https://demo9.peoplebox.biz/user/login', selector: '._main_1p1ww_22'}
+    { url: 'https://demo.peoplebox.biz/user/login', selector: '._main_1p1ww_22', errorMessage: '🚨  DO SUNUCUSUNU KONTROL EDİN!  🚨' },
+    { url: 'https://demo2.peoplebox.biz/user/login', selector: '._main_1p1ww_22414', errorMessage: '🚨  SH2 SUNUCUSUNU KONTROL EDİN!  🚨' },
+    { url: 'https://demo9.peoplebox.biz/user/login', selector: '._main_1p1ww_22', errorMessage: '🚨  KNET SUNUCUSUNU KONTROL EDİN!  🚨' }
   ];
+
+  let failedDomains = [];
 
   domains.forEach((domain) => {
     it(`Checking ${domain.url}`, () => {
       cy.visit(domain.url, { failOnStatusCode: false });
-      cy.wait(3000)
+      cy.wait(3000);
 
       cy.get('body').then(($body) => {
         if ($body.find(domain.selector).length === 0) {
-          const errorMessage = `❌ ${domain.url} is missing critical element: ${domain.selector}`;
-          cy.log(errorMessage);
+          cy.log(domain.errorMessage);
+          failedDomains.push(domain.errorMessage); // Hata mesajını listeye ekle
 
           // Hata olursa e-posta bildirimi gönder
-          exec(`node sendMail.js "${errorMessage}"`, (err, stdout, stderr) => {
+          exec(`node sendMail.js "${domain.errorMessage}"`, (err, stdout, stderr) => {
             if (err) {
               console.error('E-posta gönderilemedi:', err);
             } else {
@@ -30,5 +32,22 @@ describe('Domain Content Check', () => {
         }
       });
     });
+  });
+
+  after(() => {
+    // Eğer birden fazla domain patlarsa, bunları kontrol et ve hata mesajı oluştur
+    if (failedDomains.length > 0) {
+      const errorMessage = `⚠️ Şu domainlerde sorun tespit edildi: \n${failedDomains.join('\n')}`;
+      cy.log(errorMessage);
+
+      // E-posta bildirimi gönder
+      exec(`node sendMail.js "${errorMessage}"`, (err, stdout, stderr) => {
+        if (err) {
+          console.error('E-posta gönderilemedi:', err);
+        } else {
+          console.log('E-posta gönderildi:', stdout);
+        }
+      });
+    }
   });
 });
