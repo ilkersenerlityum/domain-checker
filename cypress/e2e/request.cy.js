5fa68515a5bcaf47f5d9edb993052a3d90c9ae9b
ilkersenerlityum <ilker@lityum.com>
2025-03-14 15:04:51 +0300

    request update11

--- cypress/e2e/request.cy.js
+++ cypress/e2e/request.cy.js
@@ -2,17 +2,13 @@ const { exec } = require('child_process');
 
 describe('Domain Content Check', () => {
   const domains = [
-    { url: 'https://demo.peoplebox.biz/user/login', selector: '._main_1p1ww_22', errorMessage: '🚨  DO SUNUCUSUNU KONTROL EDİN!  🚨' },
-    { url: 'https://demo2.peoplebox.biz/user/login', selector: '._main_1p1ww_22', errorMessage: '🚨  SH2 SUNUCUSUNU KONTROL EDİN!  🚨' },
-    { url: 'https://demo9.peoplebox.biz/user/login', selector: '._main_1p1ww_22', errorMessage: '🚨  KNET SUNUCUSUNU KONTROL EDİN!  🚨' }
+    { url: 'https://demo.peoplebox.biz/user/login', selector: '._main_1p1ww_22', errorMessage: '🚨  DO SUNUCUSU PATLADI!  🚨' },
+    { url: 'https://demo2.peoplebox.biz/user/login', selector: '._main_1p1ww_221414', errorMessage: '🚨  SH2 SUNUCUSU PATLADI!  🚨' },
+    { url: 'https://demo9.peoplebox.biz/user/login', selector: '._main_1p1ww_22', errorMessage: '🚨  KNET SUNUCUSU PATLADI!  🚨' }
   ];
 
-  let failedDomains = [];
+  const webhookURL = "https://chat.googleapis.com/v1/spaces/AAAAwMjP3Sw/messages?key=API_KEY&token=TOKEN";
 
-  require('dotenv').config();
-
-  const webhookURL = process.env.GOOGLE_CHAT_WEBHOOK_URL;
-  
   domains.forEach((domain) => {
     it(`Checking ${domain.url}`, () => {
       cy.visit(domain.url, { failOnStatusCode: false });
@@ -21,9 +17,11 @@ describe('Domain Content Check', () => {
       cy.get('body').then(($body) => {
         if ($body.find(domain.selector).length === 0) {
           cy.log(domain.errorMessage);
-          failedDomains.push(domain.errorMessage);
-
-          const chatMessage = `⚠️ *Domain Hatası!* \n${domain.errorMessage}`;
+          
+          // Hata mesajını domain ile birlikte oluştur
+          const chatMessage = `⚠️ *Domain Hatası!* \n${domain.errorMessage}\n🌐 *Hatalı URL:* ${domain.url}`;
+          
+          // Google Chat'e mesaj gönderme
           const curlCommand = `curl -X POST -H "Content-Type: application/json" -d "{\\"text\\": \\"${chatMessage}\\"}" "${webhookURL}"`;
 
           exec(curlCommand, (err, stdout, stderr) => {
@@ -38,22 +36,5 @@ describe('Domain Content Check', () => {
         }
       });
     });
-  });
-
-  after(() => {
-    if (failedDomains.length > 0) {
-      const errorMessage = `⚠️ *Domain Hataları Tespit Edildi!* \n${failedDomains.join('\n')}`;
-      cy.log(errorMessage);
-
-      const curlCommand = `curl -X POST -H "Content-Type: application/json" -d "{\\"text\\": \\"${errorMessage}\\"}" "${webhookURL}"`;
-
-      exec(curlCommand, (err, stdout, stderr) => {
-        if (err) {
-          console.error('❌ Google Chat mesajı gönderilemedi:', err);
-        } else {
-          console.log('✅ Google Chat mesajı gönderildi:', stdout);
-        }
-      });
-    }
   });
 });
