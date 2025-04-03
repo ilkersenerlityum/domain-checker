eb646251d7e26e1c3e1409cbeeb261c3676243cb
ilkersenerlityum <ilker@lityum.com>
2025-04-04 00:09:20 +0300

    request update28

--- cypress/e2e/request.cy.js
+++ cypress/e2e/request.cy.js
@@ -8,12 +8,12 @@ describe("Domain Content Check", () => {
       errorMessage: "🚨 DO SUNUCUSUNU KONTROL EDİN!",
     },
     {
-      url: "https://demo2.peoplebox.biz/user/login",
+      url: "https://demo2123123.peoplebox.biz/user/login",
       selector: "._main_1p1ww_22",
       errorMessage: "🚨 SH2 SUNUCUSUNU KONTROL EDİN!",
     },
     {
-      url: "https://demo9.peoplebox.biz/user/login",
+      url: "https://demo9123123.peoplebox.biz/user/login",
       selector: "._main_1p1ww_22",
       errorMessage: "🚨 KNET SUNUCUSUNU KONTROL EDİN!",
     },
@@ -23,7 +23,6 @@ describe("Domain Content Check", () => {
 
   domains.forEach((domain) => {
     it(`Checking ${domain.url}`, () => {
-      // Sayfa HTTP hatası veriyor mu kontrol ediyoruz
       cy.request({
         url: domain.url,
         failOnStatusCode: false,
@@ -31,17 +30,16 @@ describe("Domain Content Check", () => {
         if (response.status >= 400) {
           const errorMsg = `🚨 ${domain.url} açılırken hata aldı: ${response.status} → ${domain.errorMessage}`;
           cy.log(errorMsg);
-          failedDomains.push(`🌐 ${domain.url} → ${errorMsg}`);
+          failedDomains.push(domain.errorMessage);
           throw new Error(errorMsg);
         } else {
           cy.visit(domain.url, { failOnStatusCode: false });
           cy.wait(3000);
 
-          // Selector'un olup olmadığını kontrol ediyoruz
           cy.get("body").then(($body) => {
             if ($body.find(domain.selector).length === 0) {
               cy.log(domain.errorMessage);
-              failedDomains.push(`🌐 ${domain.url} → ${domain.errorMessage}`);
+              failedDomains.push(domain.errorMessage);
               throw new Error(`🚨 HATA: ${domain.url} → ${domain.errorMessage}`);
             } else {
               cy.log(`✅ ${domain.url} is OK!`);
@@ -54,13 +52,8 @@ describe("Domain Content Check", () => {
 
   after(() => {
     if (failedDomains.length > 0) {
-      console.log("🔥 HATALI DOMAINLER:");
-      failedDomains.forEach((msg) => console.log(msg));
-
       const errorMessage = `⚠️ *Domain Hataları Tespit Edildi!* \n${failedDomains.join("\n")}`;
       cy.log(errorMessage);
-
-      // Cypress hataları yakalaması için bir error fırlatıyoruz
       throw new Error(errorMessage);
     }
   });
