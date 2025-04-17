ac8b7e6a6c567afd858e65848b04e4d064b74e6f
ilkersenerlityum <ilker@lityum.com>
2025-04-17 14:24:50 +0300

    request update49

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,5 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      on("task", {
-        logToTerminal(message) {
-          console.log(message);
-          return null;
-        },
-      });
-    },
-  },
+  e2e: {},
 });
